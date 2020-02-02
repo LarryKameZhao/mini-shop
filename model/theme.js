6496aa84149f615dc50b504dc19014b224a43450
LarryKameZhao <larryzhao1994@outlook.com>
2020-02-02 15:59:07 +0800

    await wx.request

---
+++ model/theme.js
@@ -0,0 +1,13 @@
+import { Http } from "../utils/http";
+class Theme {
+  static async getHomeLocationA() {
+    return await Http.request({
+      url: "theme/by/names",
+      method: "GET",
+      data: {
+        names: "t-1"
+      }
+    });
+  }
+}
+export { Theme };
